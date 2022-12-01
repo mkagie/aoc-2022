7b26a05768b0ac6beb86880a676ddd29cdebdc9b
Matt Kagie <mkagie@anduril.com>
2022-11-30 19:02:05 -0800

    Added more to the template

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,6 +1,46 @@
 //! Command line executable for running part one and part two
+use std::{fs::File, io::BufReader};
+
 use clap::Parser;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Input file
+    #[arg(short)]
+    input_file: String,
+
+    #[command(subcommand)]
+    part: Part,
+}
+
+#[derive(clap::Subcommand, Debug)]
+enum Part {
+    Part1,
+    Part2,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let file = BufReader::new(File::open(args.input_file).expect("Cannot find file"));
+
+    let answer = match args.part {
+        Part::Part1 => part_one(&file),
+        Part::Part2 => part_two(&file),
+    };
+
+    println!("{:?}", answer);
+}
+
+fn part_one<T>(file: &BufReader<T>) {}
+
+fn part_two<T>(file: &BufReader<T>) {}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_one() {}
+
+    fn test_two() {}
 }
