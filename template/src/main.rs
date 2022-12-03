3f93f2d5920679463201e648cfd97ad18181d26d
Matt Kagie <mjkagie@gmail.com>
2022-12-03 10:17:11 -0800

    Applied a different structure to the template going forward

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,5 +1,8 @@
 //! Command line executable for running part one and part two
-use std::{fs::File, io::{BufReader, BufRead}};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 use clap::Parser;
 
@@ -34,23 +37,34 @@ fn main() {
 }
 
 fn part_one(file: BufReader<File>) -> ReturnType {
-    let input = parse_input(file);
+    let input = parse_input(file, map_one);
     part_one_internal(input)
 }
 
 fn part_two(file: BufReader<File>) -> ReturnType {
-    let input = parse_input(file);
+    let input = parse_input(file, map_two);
     part_two_internal(input)
+}
+
+fn parse_input<F, T>(file: BufReader<File>, f: F) -> Vec<T>
+where
+    F: Fn(&str) -> T,
+{
+    file.lines().map(|x| f(x.unwrap())).collect()
 }
 
 // TODO -- Update this with the return type
 type ReturnType = u64;
 type VectorType = u32;
+type VectorType2 = u32;
 
+/// Map a line to a VectorType
+fn map_one(input: &str) -> VectorType {
+    todo!()
+}
 
-// TODO Implement this
-fn parse_input(file: BufReader<File>) -> Vec<VectorType> {
-    let input: Vec<String> = file.lines().map(|x| x.unwrap()).collect();
+/// Map a line to a VectorType
+fn map_two(input: &str) -> VectorType {
     todo!()
 }
 
@@ -60,7 +74,7 @@ fn part_one_internal(input: Vec<VectorType>) -> ReturnType {
 }
 
 // TODO Implement this
-fn part_two_internal(input: Vec<VectorType>) -> ReturnType {
+fn part_two_internal(input: Vec<VectorType2>) -> ReturnType {
     todo!()
 }
 
