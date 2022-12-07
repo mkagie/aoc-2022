edeea720e53a312ca1782640d4dfb31075b843cc
Matt Kagie <mjkagie@gmail.com>
2022-12-06 23:15:44 -0800

    Finished

--- day-seven/src/main.rs
+++ day-seven/src/main.rs
@@ -4,7 +4,10 @@ use std::{
     io::{BufRead, BufReader},
 };
 
+use anyhow::{anyhow, Result};
 use clap::Parser;
+
+use id_tree::{InsertBehavior, Node, NodeId, Tree};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -54,28 +57,237 @@ where
 }
 
 // TODO -- Update this with the return type
-type ReturnType = u64;
-type VectorType = u32;
-type VectorType2 = u32;
+type ReturnType = usize;
+type VectorType = String;
+type VectorType2 = String;
+
+#[derive(Debug, Clone)]
+struct LocalFile {
+    size: usize,
+    _name: String,
+}
+impl LocalFile {
+    fn try_new(input: &str) -> Result<Self> {
+        let mut words = input.split_whitespace();
+        let first = words.next().unwrap();
+        if first != "dir" {
+            let size: usize = first.parse().unwrap();
+            let name = words.next().unwrap().to_string();
+            Ok(Self { size, _name: name })
+        } else {
+            Err(anyhow!("Not a file"))
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Directory {
+    name: String,
+    files: Vec<LocalFile>,
+    pub directory_data_size: usize, // Ignore directories for now, as that is going to be a new node
+}
+impl Directory {
+    fn try_new(input: &str) -> Result<Self> {
+        let mut words = input.split_whitespace();
+        let first = words.next().unwrap();
+        if first == "dir" {
+            let name = words.next().unwrap().to_string();
+            Ok(Self {
+                name,
+                files: Vec::new(),
+                directory_data_size: 0,
+            })
+        } else {
+            Err(anyhow!("Not a file"))
+        }
+    }
+
+    fn new(name: &str) -> Self {
+        Self {
+            name: name.to_string(),
+            files: Vec::new(),
+            directory_data_size: 0,
+        }
+    }
+
+    fn add_file(&mut self, file: LocalFile) {
+        self.files.push(file);
+    }
+
+    fn get_file_sizes(&self) -> usize {
+        self.files.iter().map(|x| x.size).sum()
+    }
+
+    fn add_sizes_from_below(&mut self, size: usize) {
+        self.directory_data_size += size;
+    }
+
+    fn get_full_size(&self) -> usize {
+        self.directory_data_size + self.get_file_sizes()
+    }
+}
+
+#[derive(Debug)]
+enum Command {
+    Cd(String),
+    Ls,
+}
+impl Command {
+    fn try_new(input: &str) -> Result<Self> {
+        let mut words = input.split_whitespace();
+        if words.next().unwrap() == "$" {
+            let cmd = words.next().unwrap();
+            if cmd == "cd" {
+                Ok(Command::Cd(words.next().unwrap().to_string()))
+            } else {
+                Ok(Command::Ls)
+            }
+        } else {
+            Err(anyhow!("Not a command"))
+        }
+    }
+}
 
 /// Map a line to a VectorType
 fn map_one(input: &str) -> VectorType {
-    todo!()
+    input.to_string()
 }
 
 /// Map a line to a VectorType
 fn map_two(input: &str) -> VectorType2 {
-    todo!()
+    input.to_string()
+}
+
+fn build_tree(input: Vec<VectorType>) -> Tree<Directory> {
+    let mut tree = Tree::new();
+    // First, create a directory
+    let dir = Directory::new("/");
+    let mut node_id = tree.insert(Node::new(dir), InsertBehavior::AsRoot).unwrap();
+    for line in input {
+        // Check to see if this is a command
+        if let Ok(cmd) = Command::try_new(&line) {
+            match cmd {
+                Command::Cd(name) => {
+                    if name == "/" {
+                        // Do nothing, we did it above
+                    } else if name == ".." {
+                        // Move backwards
+                        node_id = tree.get(&node_id).unwrap().parent().unwrap().clone();
+                    } else {
+                        // Find the node ID of your children
+                        node_id = tree
+                            .get(&node_id)
+                            .unwrap()
+                            .children()
+                            .iter()
+                            .find(|child| tree.get(child).unwrap().data().name == name)
+                            .unwrap()
+                            .clone();
+                    }
+                }
+                Command::Ls => {}
+            }
+        } else if let Ok(dir) = Directory::try_new(&line) {
+            // Add it to the tree, do not save node_id
+            let _ = tree
+                .insert(Node::new(dir), InsertBehavior::UnderNode(&node_id))
+                .unwrap();
+        } else if let Ok(file) = LocalFile::try_new(&line) {
+            // Add the file to the directory
+            tree.get_mut(&node_id).unwrap().data_mut().add_file(file);
+        }
+    }
+    tree
 }
 
 // TODO Implement this
 fn part_one_internal(input: Vec<VectorType>) -> ReturnType {
-    todo!()
+    let mut tree = build_tree(input);
+    // Starts top to bottom
+    let mut node_ids: Vec<NodeId> = tree
+        .traverse_level_order_ids(tree.root_node_id().unwrap())
+        .unwrap()
+        .collect();
+    // Now, make bottom to top
+    node_ids.reverse();
+    // Traverse and add sizes to the trees
+    for node_id in node_ids {
+        // Get Node
+        let node = tree.get(&node_id).unwrap();
+        let dir = node.data();
+        // Compute full size of this bad boy
+        let size = dir.get_file_sizes() + dir.directory_data_size;
+        // Add this to the parent
+        let parent_id = if node.parent().is_some() {
+            Some(node.parent().unwrap().clone())
+        } else {
+            None
+        };
+        if let Some(parent_id) = parent_id {
+            tree.get_mut(&parent_id)
+                .unwrap()
+                .data_mut()
+                .add_sizes_from_below(size);
+        }
+    }
+
+    tree.traverse_level_order(tree.root_node_id().unwrap())
+        .unwrap()
+        .map(|node| node.data().get_full_size())
+        .filter(|&size| size <= 100000)
+        .sum()
 }
 
 // TODO Implement this
 fn part_two_internal(input: Vec<VectorType2>) -> ReturnType {
-    todo!()
+    let mut tree = build_tree(input);
+    // Starts top to bottom
+    let mut node_ids: Vec<NodeId> = tree
+        .traverse_level_order_ids(tree.root_node_id().unwrap())
+        .unwrap()
+        .collect();
+    // Now, make bottom to top
+    node_ids.reverse();
+    // Traverse and add sizes to the trees
+    for node_id in node_ids {
+        // Get Node
+        let node = tree.get(&node_id).unwrap();
+        let dir = node.data();
+        // Compute full size of this bad boy
+        let size = dir.get_file_sizes() + dir.directory_data_size;
+        // Add this to the parent
+        let parent_id = if node.parent().is_some() {
+            Some(node.parent().unwrap().clone())
+        } else {
+            None
+        };
+        if let Some(parent_id) = parent_id {
+            tree.get_mut(&parent_id)
+                .unwrap()
+                .data_mut()
+                .add_sizes_from_below(size);
+        }
+    }
+
+    // Get total amount of space being used
+    let total_free_space = 70000000
+        - tree
+            .get(tree.root_node_id().unwrap())
+            .unwrap()
+            .data()
+            .get_full_size();
+    println!("{:?}", total_free_space);
+    let needed_space_for_update = 30000000;
+    println!("{:?}", needed_space_for_update);
+    let needed_space = needed_space_for_update - total_free_space;
+    println!("{:?}", needed_space);
+
+    tree.traverse_level_order(tree.root_node_id().unwrap())
+        .unwrap()
+        .map(|node| node.data().get_full_size())
+        .filter(|&size| size >= needed_space)
+        .min()
+        .unwrap()
 }
 
 #[cfg(test)]
