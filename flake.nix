e61490afbb3903571df8db7a923b1301609d161a
mkagie <90227310+mkagie@users.noreply.github.com>
2023-06-02 18:51:08 -0700

    Pythonic solution -- not worrying about making pretty (#15)
    * Pythonic solution
    
    * Cleaned up and merged
    
    * Added black and ran it
    
    ---------
    
    Co-authored-by: Matt Kagie <mkagie@anduril.com>

--- flake.nix
+++ flake.nix
@@ -20,34 +20,26 @@
           rust-analyzer
           cargo-generate
         ];
+
+        python = pkgs.python3.withPackages (p: with p; [
+          ipython
+          matplotlib
+          numpy
+          scipy
+          black
+        ]);
       in
       {
         defaultPackage = naersk-lib.buildPackage ./.;
-        devShell = with pkgs; mkShell {
-          buildInputs = rust-packages;
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        devShells = with pkgs; {
+          default = mkShell {
+            buildInputs = rust-packages;
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          };
+          python = mkShell {
+            buildInputs = [ python ];
+          };
         };
-
-        # checks = {
-        #   format = pkgs.runCommand "check-format"
-        #     {
-        #       buildInputs = rust-packages ++ [
-        #         pkgs.nixpkgs-fmt
-        #       ];
-        #     }
-        #     ''
-        #       ${pkgs.rustfmt}/bin/cargo-fmt fmt --manifest-path ${./.}/Cargo.toml -- --check
-        #       ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt --check ${./.}
-        #       touch $out
-        #     '';
-        #   clippy = pkgs.runCommand "check-clippy"
-        #     {
-        #       buildInputs = rust-packages;
-        #     }
-        #     ''
-        #       ${pkgs.rustPackages.clippy}/bin/cargo-clippy --all-targets --all-features -- -D warnings
-        #       touch $out
-        #     '';
-        # };
-      });
+      }
+    );
 }
